53fcef61f1dc4cb3e632c630179fd9f6220bb5bc
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2021-05-09 12:15:07 +0900

    PySimpleGUIでBLEのUI

--- test/ble/psg.py
+++ test/ble/psg.py
@@ -1,27 +1,41 @@
 import asyncio
 import time
+import numpy as np
 
 import PySimpleGUI as sg
 
 sg.theme('Dark Blue 3')
 layout = [
-    [sg.Text('Python GUI')],
-    [sg.Text('大きさ', size=(15, 1)), sg.InputText('0')],
-    [sg.Submit(button_text='Send test')],
-    [sg.Submit(button_text='Quit')]
+  [sg.T('Miraisense test GUI')],
+  [sg.T('BLE',key='BLE',background_color='#333333'),sg.T('HAP'),sg.T('IMU'),sg.T('BAT'),sg.T('---')],
+  [sg.Slider(range=(0, 255), orientation='v', size=(5, 20), default_value=0),sg.Slider(range=(-100, 100), orientation='h', size=(20, 20), default_value=0)],
+  [sg.B('TEST'),sg.Exit()]
 ]
 
-window = sg.Window(title='show input text', size=(200, 200)).Layout(layout)
+window = sg.Window(title='Controller', size=(200, 200)).Layout(layout)
 
+pvalues=""
 async def evloop():
+  global pvalues
   while True:
-    event, values = window.read()
-    print("event is ",event)
-    if event == 'Send test':
-      queue.put_nowait(b'SF I100 C120')
-    elif event == 'Quit':
+    event, values=window.read(timeout=50)
+    if event!='__TIMEOUT__': print("event is ",event)
+    if event==sg.WIN_CLOSED or event=='Exit':
       queue.put_nowait('Quit')
-    await asyncio.sleep(0.1)
+      break
+    elif event=='TEST':
+      window['BLE'].update(background_color='#00FFFF')
+    if str(values)!=pvalues:
+      print(values,pvalues)
+      r=values[0]
+      if r>0:
+        y=r*values[1]/100
+        a=np.arcsin(-y/r)*180/3.1415
+        queue.put_nowait('SF I'+str(int(r))+' C'+str(int(a)))
+      else:
+        queue.put_nowait('SF I0')
+      pvalues=str(values)
+    await asyncio.sleep(0.05)
 
 from bleak import BleakClient
 from bleak import discover
@@ -47,14 +61,13 @@ async def bl_connect(name):
     print("Pairing")
     paired = await client.pair(protection_level=2)
     print("Connected")
-    await client.write_gatt_char(CWR_UUID,b'SF I100 C120')
+    window['BLE'].update(background_color='#00FFFF')
     while True:
         cmd = await queue.get()
         print('CMD:',cmd)
-        if cmd is 'Quit': break
-        await client.write_gatt_char(CWR_UUID,cmd)
+        if cmd=='Quit': break
+        await client.write_gatt_char(CWR_UUID,cmd.encode('utf-8'))
         queue.task_done()
-    print("Connected")
 
 async def main():
   global queue
@@ -64,6 +77,5 @@ async def main():
   task2= asyncio.create_task(evloop())
   await task1
   await task2
-  root.update()
 
 asyncio.run(main())
